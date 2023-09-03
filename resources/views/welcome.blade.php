f7896b5d6286b3857e7ce45a4ee9274933ab6d56
Harsha Madhawa <112013732+HarshaMadhawa@users.noreply.github.com>
2023-09-02 22:30:29 -0700

    1st commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,101 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+    <link rel="stylesheet" href="cssFile/homePage.css">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=`, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+
+<body>
+
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <div class="container-fluid">
+            <img id="logo" src="image/logo12.png" alt="ruhuna" />
+
+            <p>University Of Ruhuna <br>
+                Department Of Computer Science </p>
+
+
+            <a class="navbar-brand ps-3 " href="homePage">Home</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+                aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="login">Log In</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="signin">Sign In</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="login">Log In</a>
+                    </li>
+
+
+                </ul>
+
+            </div>
+        </div>
+    </nav>
+
+
+
+    <div id="carouselExampleAutoplaying" class="carousel slide" data-bs-ride="carousel">
+        <a> Welcome to the Department of Computer Science's Alumni Network at
+            the University of Ruhuna. Our journey begins with a shared passion for
+            technology and innovation, uniting alumni who have made their mark in
+            the ever-evolving world of computer science. Here, we celebrate our
+            collective achievements, foster connections, and support the next
+            generation of computer scientists. Join us in preserving the legacy,
+            strengthening bonds, and embracing the limitless potential of our alumni community.</a>
+        <div class="carousel-inner">
+            <div class="carousel-item active img1">
+                <img src="image/img1.jpg" class="d-block w-100" alt="img1.jpg">
+            </div>
+            <div class="carousel-item img2">
+                <img src="image/img2.jpg" class="d-block w-100" alt="img2.jpg">
+            </div>
+            <div class="carousel-item img3">
+                <img src="image/img3.jpg" class="d-block w-100" alt="img3.jpg">
+            </div>
+        </div>
+        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying"
+            data-bs-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Previous</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleAutoplaying"
+            data-bs-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Next</span>
+        </button>
+    </div>
+
+
+
+
+
+
+
+
+    <!-- <div class="cslab-image">
+        <img src="image/banner.jpg" class="img-fluid" alt="cslab">
+    </div> -->
+
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"
+        integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r"
+        crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.min.js"
+        integrity="sha384-Rx+T1VzGupg4BHQYs2gCW9It+akI2MM/mndMCy36UVfodzcJcF0GGLxZIzObiEfa"
+        crossorigin="anonymous"></script>
+</body>
+
+</html>
